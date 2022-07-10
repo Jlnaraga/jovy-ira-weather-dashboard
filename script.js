f28e7e2b766ac6d1f1fcebe792bac91f215cc707
Jovy Ira <ira_bonching@yahoo.com>
2022-07-10 19:58:52 -0400

    added comments

---
+++ script.js
@@ -0,0 +1,171 @@
+/*## Acceptance Criteria
+
+```
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+WHEN I view the UV index
+THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+WHEN I view future weather conditions for that city*/
+
+var apiKey = "d1e2d0763204896fd894698f5c6e27ee";
+var today = moment().format('L');
+var searchHistoryList = [];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* WHEN I view current weather conditions for that city
+THEN I am presented with the city name
+the date
+an icon representation of weather conditions
+the temperature
+the humidity
+the wind speed*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* WHEN I view the UV index
+THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+0-2 green#3EA72D, 3-5 yellow#FFF300, 6-7 orange#F18B00, 8-10 red#E53210, 11+violet#B567A4*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function for future condition
+
+
+
+
+
+// THEN I am presented with a 5-day forecast
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*displays the date
+an icon representation of weather conditions
+the temperature
+the humidity*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+
+
+
+
+
+// WHEN I open the weather dashboard
+// THEN I am presented with the last searched city forecast
+
